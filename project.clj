bd6c1c5227bb6f4eacf24bce433ddd385c876b44
David Dinh <david@dinh.io>
2014-01-21 09:19:49 -0800

    Added lein template

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject trustee "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot trustee.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
