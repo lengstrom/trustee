71ca64bda3e0b47a67daeb3e0b344d71a0b557de
David Dinh <david@dinh.io>
2014-01-21 09:25:06 -0800

    Fixed license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject trustee "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD (3-clause) License"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot trustee.core
   :target-path "target/%s"
