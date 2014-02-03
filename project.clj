eb47f83f5ebd5168c210d29073fb407177b213ca
David Dinh <david@dinh.io>
2014-02-02 17:51:01 -0800

    Added clojurescript as dep; changed license to gpl3

--- project.clj
+++ project.clj
@@ -1,10 +1,21 @@
 (defproject trustee "0.1.0-SNAPSHOT"
   :description "An offline documentation viewer"
   :url "https://github.com/ddinh/trustee"
-  :license {:name "BSD (3-clause) License"
-            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :license {:name "GPL3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [seesaw "1.4.0"]]
-  :main ^:skip-aot trustee.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [seesaw "1.4.4"]
+                 [org.clojure/clojurescript "0.0-2138"
+                  :exclusions [org.apache.ant/ant]]
+                 ]
+  :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]d
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:optimizations :simple
+                                   :externs ["externs/jquery.js" "externs/throttle.js" "externs/codemirror.js"]
+                                   :source-map "deploy/core/node_modules/trustee/bootstrap.js.map"
+                                   :output-to "deploy/core/node_modules/trustee/bootstrap.js"
+                                   :output-dir "deploy/core/node_modules/trustee/cljs/"
+                                   :pretty-print true}}]}
+  :plugins [[lein-cljsbuild "1.0.1"]]
+  :source-paths ["src/"]
+)
