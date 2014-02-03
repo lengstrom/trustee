92d47c2ab9396691fc718f9463b33e1172881a4e
David Dinh <david@dinh.io>
2014-02-02 18:23:09 -0800

    Removed seesaw from deps

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :license {:name "GPL3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [seesaw "1.4.4"]
                  [org.clojure/clojurescript "0.0-2138"
                   :exclusions [org.apache.ant/ant]]
                  ]
-  :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]d
+  :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:optimizations :simple
                                    :externs ["externs/jquery.js" "externs/throttle.js" "externs/codemirror.js"]
